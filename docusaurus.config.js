f740afaf81d4916edff4739e145f1ab27bcd6469
Adrian Djura <adrian.djura@gmail.com>
2021-01-13 22:08:44 +0100

    docusaurus setup

---
+++ docusaurus.config.js
@@ -0,0 +1,59 @@
+module.exports = {
+  title: 'ScioXR',
+  tagline: 'XR platform for creating and playing content',
+  url: 'https://scioxr.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'flathillgames', // Usually your GitHub org/user name.
+  projectName: 'scioxr', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      logo: {
+        alt: 'ScioXR Logo',
+        src: 'img/logo_horizontal.png',
+      },
+      items: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        {to: 'blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://github.com/flathillgames/ScioXR',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      copyright: `Copyright © ${new Date().getFullYear()} ScioXR, Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/flathillgames/flathillgames.github.io/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/flathillgames/flathillgames.github.io/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
